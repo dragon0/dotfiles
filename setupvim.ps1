45a3eccd03d06459d1135533310fdea086d5ce80
Raven <1137285+dragon0@users.noreply.github.com>
2018-02-12 18:36:56 -0800

    Create setupvim.ps1

---
+++ setupvim.ps1
@@ -0,0 +1,4 @@
+cd $env:userprofile
+curl https://raw.githubusercontent.com/dragon0/dotfiles/master/.vimrc -o .vimrc
+curl https://raw.githubusercontent.com/dragon0/dotfiles/master/.gvimrc -o .gvimrc
+mkdir .swp
